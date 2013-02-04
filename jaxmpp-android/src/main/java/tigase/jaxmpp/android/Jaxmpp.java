50e69212b596bc99fa9d016715c64bf613f890ca
Bartosz Malkowski <bmalkowski@tigase.pl>
2013-02-04 19:19:22 +0100

    add library version for Android

---
+++ jaxmpp-android/src/main/java/tigase/jaxmpp/android/Jaxmpp.java
@@ -0,0 +1,15 @@
+package tigase.jaxmpp.android;
+
+import tigase.jaxmpp.core.client.SessionObject;
+
+public class Jaxmpp extends tigase.jaxmpp.j2se.Jaxmpp {
+
+	public Jaxmpp() {
+		super();
+	}
+
+	public Jaxmpp(SessionObject sessionObject) {
+		super(sessionObject);
+	}
+
+}
