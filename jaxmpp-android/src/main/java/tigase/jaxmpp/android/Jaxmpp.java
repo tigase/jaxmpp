d363bfb591c49915374e5da17a7f95f1743e09ca
Bartosz Malkowski <bmalkowski@tigase.pl>
2013-02-05 13:20:15 +0100

    add Android compatible DNS resolver

--- jaxmpp-android/src/main/java/tigase/jaxmpp/android/Jaxmpp.java
+++ jaxmpp-android/src/main/java/tigase/jaxmpp/android/Jaxmpp.java
@@ -1,8 +1,20 @@
 package tigase.jaxmpp.android;
 
 import tigase.jaxmpp.core.client.SessionObject;
+import tigase.jaxmpp.core.client.factory.UniversalFactory;
+import tigase.jaxmpp.core.client.factory.UniversalFactory.FactorySpi;
+import tigase.jaxmpp.j2se.connectors.socket.SocketConnector.DnsResolver;
 
 public class Jaxmpp extends tigase.jaxmpp.j2se.Jaxmpp {
+	static {
+		UniversalFactory.setSpi(DnsResolver.class.getName(), new FactorySpi<DnsResolver>() {
+
+			@Override
+			public DnsResolver create() {
+				return new AndroidDNSResolver();
+			}
+		});
+	}
 
 	public Jaxmpp() {
 		super();
