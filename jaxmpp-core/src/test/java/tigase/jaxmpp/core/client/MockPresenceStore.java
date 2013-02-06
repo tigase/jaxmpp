6bd78680e9a8283ffb27f44f4d89c85caabf3a2e
Bartosz Malkowski <bmalkowski@tigase.pl>
2013-02-06 16:39:10 +0100

    #1005 -- Presence module is not thread safe

--- jaxmpp-core/src/test/java/tigase/jaxmpp/core/client/MockPresenceStore.java
+++ jaxmpp-core/src/test/java/tigase/jaxmpp/core/client/MockPresenceStore.java
@@ -3,8 +3,6 @@ package tigase.jaxmpp.core.client;
 import java.util.HashMap;
 import java.util.Map;
 
-import tigase.jaxmpp.core.client.BareJID;
-import tigase.jaxmpp.core.client.JID;
 import tigase.jaxmpp.core.client.xmpp.modules.presence.PresenceStore;
 import tigase.jaxmpp.core.client.xmpp.stanzas.Presence;
 
@@ -15,5 +13,10 @@ public class MockPresenceStore extends PresenceStore {
 		presenceByJid = new HashMap<JID, Presence>();
 		bestPresence = new HashMap<BareJID, Presence>();
 	}
-	
+
+	@Override
+	protected Map<String, Presence> createResourcePresenceMap() {
+		return new HashMap<String, Presence>();
+	}
+
 }
