98419a11400b91c07b007cdda9a07d4490c4c38d
Bartosz Malkowski <bmalkowski@tigase.pl>
2015-06-01 09:54:10 +0200

    Support #3056 add: test for getAsString()

--- jaxmpp-core/src/test/java/tigase/jaxmpp/core/client/xml/DefaultElementTest.java
+++ jaxmpp-core/src/test/java/tigase/jaxmpp/core/client/xml/DefaultElementTest.java
@@ -17,106 +17,114 @@
  */
 package tigase.jaxmpp.core.client.xml;
 
-import java.util.List;
-
 import junit.framework.TestCase;
+
+import java.util.List;
 
 public class DefaultElementTest extends TestCase {
 
-	private static final Element createElement() throws XMLException {
-		Element message = ElementFactory.create("message", null, null);
-		message.setAttribute("to", "romeo@example.net");
-		message.setAttribute("from", "juliet@example.com/balcony");
-		message.setAttribute("type", "chat");
+    private static Element createElement() throws XMLException {
+        Element message = ElementFactory.create("message", null, null);
+        message.setAttribute("to", "romeo@example.net");
+        message.setAttribute("from", "juliet@example.com/balcony");
+        message.setAttribute("type", "chat");
 
-		message.addChild(ElementFactory.create("subject", "I implore you!", null));
-		message.addChild(ElementFactory.create("body", "Wherefore art thou, Romeo?", null));
-		message.addChild(ElementFactory.create("thread", "e0ffe42b28561960c6b12b944a092794b9683a38", null));
-		message.addChild(ElementFactory.create("x", "tigase:offline", "tigase"));
+        message.addChild(ElementFactory.create("subject", "I implore you!", null));
+        message.addChild(ElementFactory.create("body", "Wherefore art thou, Romeo?", null));
+        message.addChild(ElementFactory.create("thread", "e0ffe42b28561960c6b12b944a092794b9683a38", null));
+        message.addChild(ElementFactory.create("x", "tigase:offline", "tigase"));
 
-		return message;
-	}
+        return message;
+    }
 
-	public void testGetAttribute() throws XMLException {
-		final Element element = createElement();
+    public void testGetAsString() throws XMLException {
+        Element auth = ElementFactory.create("auth");
+        auth.setAttribute("xmlns", "urn:ietf:params:xml:ns:xmpp-sasl");
+        auth.setValue("base64content");
 
-		assertEquals("juliet@example.com/balcony", element.getAttribute("from"));
-		assertEquals("romeo@example.net", element.getAttribute("to"));
-	}
+        assertEquals("<auth xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\">base64content</auth>", auth.getAsString());
+    }
 
-	public void testGetChildren() throws XMLException {
-		final Element element = createElement();
-		assertEquals("message", element.getName());
+    public void testGetAttribute() throws XMLException {
+        final Element element = createElement();
 
-		List<Element> c = element.getChildren("subject");
-		assertEquals(1, c.size());
-		Element e = c.get(0);
-		assertEquals("subject", e.getName());
+        assertEquals("juliet@example.com/balcony", element.getAttribute("from"));
+        assertEquals("romeo@example.net", element.getAttribute("to"));
+    }
 
-		c = element.getChildrenNS("tigase");
-		assertEquals(1, c.size());
-		e = c.get(0);
-		assertEquals("x", e.getName());
+    public void testGetChildren() throws XMLException {
+        final Element element = createElement();
+        assertEquals("message", element.getName());
 
-		Element c1 = element.getChildrenNS("x", "tigase");
-		assertNotNull(c1);
-		assertEquals("x", c1.getName());
+        List<Element> c = element.getChildren("subject");
+        assertEquals(1, c.size());
+        Element e = c.get(0);
+        assertEquals("subject", e.getName());
 
-	}
+        c = element.getChildrenNS("tigase");
+        assertEquals(1, c.size());
+        e = c.get(0);
+        assertEquals("x", e.getName());
 
-	public void testGetChildrenAfter() throws XMLException {
-		final Element element = createElement();
+        Element c1 = element.getChildrenNS("x", "tigase");
+        assertNotNull(c1);
+        assertEquals("x", c1.getName());
 
-		List<Element> c = element.getChildren("subject");
-		assertEquals(1, c.size());
-		Element e = c.get(0);
-		Element body = element.getChildAfter(e);
-		assertEquals("body", body.getName());
-		assertEquals("Wherefore art thou, Romeo?", body.getValue());
-	}
+    }
 
-	public void testGetFirstChild() throws XMLException {
-		final Element element = createElement();
-		Element fc = element.getFirstChild();
+    public void testGetChildrenAfter() throws XMLException {
+        final Element element = createElement();
 
-		assertEquals("subject", fc.getName());
-	}
+        List<Element> c = element.getChildren("subject");
+        assertEquals(1, c.size());
+        Element e = c.get(0);
+        Element body = element.getChildAfter(e);
+        assertEquals("body", body.getName());
+        assertEquals("Wherefore art thou, Romeo?", body.getValue());
+    }
 
-	public void testGetNextSibling() throws XMLException {
-		final Element element = createElement();
+    public void testGetFirstChild() throws XMLException {
+        final Element element = createElement();
+        Element fc = element.getFirstChild();
 
-		List<Element> c = element.getChildren("subject");
-		assertEquals(1, c.size());
-		Element e = c.get(0);
-		Element body = e.getNextSibling();
-		assertEquals("body", body.getName());
-		assertEquals("Wherefore art thou, Romeo?", body.getValue());
-	}
+        assertEquals("subject", fc.getName());
+    }
 
-	public void testGetXMLNS() throws XMLException {
-		final Element element = createElement();
+    public void testGetNextSibling() throws XMLException {
+        final Element element = createElement();
 
-		assertNull(element.getXMLNS());
+        List<Element> c = element.getChildren("subject");
+        assertEquals(1, c.size());
+        Element e = c.get(0);
+        Element body = e.getNextSibling();
+        assertEquals("body", body.getName());
+        assertEquals("Wherefore art thou, Romeo?", body.getValue());
+    }
 
-		Element c = element.getChildrenNS("x", "tigase");
-		assertNotNull(c);
-		assertEquals("x", c.getName());
-		assertEquals("tigase", c.getXMLNS());
+    public void testGetXMLNS() throws XMLException {
+        final Element element = createElement();
 
-	}
+        assertNull(element.getXMLNS());
 
-	public void testFindChild() throws XMLException {
-		final Element element = createElement();
+        Element c = element.getChildrenNS("x", "tigase");
+        assertNotNull(c);
+        assertEquals("x", c.getName());
+        assertEquals("tigase", c.getXMLNS());
 
-		Element nullElement = element.findChild( new String[] {"message", "missing"} );
+    }
 
-		assertNull(nullElement);
+    public void testFindChild() throws XMLException {
+        final Element element = createElement();
 
-		Element c = element.findChild( new String[] {"message", "body"} );
-		assertNotNull(c);
-		assertEquals("body", c.getName());
-		assertEquals("Wherefore art thou, Romeo?", c.getValue());
+        Element nullElement = element.findChild(new String[]{"message", "missing"});
 
-	}
+        assertNull(nullElement);
+
+        Element c = element.findChild(new String[]{"message", "body"});
+        assertNotNull(c);
+        assertEquals("body", c.getName());
+        assertEquals("Wherefore art thou, Romeo?", c.getValue());
+
+    }
 
 }
