6f837ef8d05a3b600961d6c44e5ad1d12a9f54f0
Bartosz Malkowski <bmalkowski@tigase.pl>
2016-06-28 11:56:03 +0200

    update: jaxmpp-samples
    add MUC invotation handlers

--- jaxmpp-samples/src/main/java/muc/RoomJoinSample.java
+++ jaxmpp-samples/src/main/java/muc/RoomJoinSample.java
@@ -1,6 +1,7 @@
 package muc;
 
 import tigase.jaxmpp.core.client.BareJID;
+import tigase.jaxmpp.core.client.JID;
 import tigase.jaxmpp.core.client.SessionObject;
 import tigase.jaxmpp.core.client.exceptions.JaxmppException;
 import tigase.jaxmpp.core.client.xml.XMLException;
@@ -88,15 +89,41 @@ public class RoomJoinSample {
 			@Override
 			public void onYouJoined(SessionObject sessionObject, Room room, String asNickname) {
 				System.out.println("You joined to room!");
+
+				// sending sample message
 				try {
 					room.sendMessage("test");
 				} catch (JaxmppException e) {
+					e.printStackTrace();
+				}
 
-
+				// inviting
+				try {
+					mucModule.invite(room, JID.jidInstance("somebody@somewhere.com"), "I want to invite you");
+				} catch (JaxmppException e) {
+					e.printStackTrace();
 				}
 			}
 		});
+
+		jaxmpp.getEventBus().addHandler(MucModule.InvitationReceivedHandler.InvitationReceivedEvent.class, new MucModule.InvitationReceivedHandler() {
+			@Override
+			public void onInvitationReceived(SessionObject sessionObject, MucModule.Invitation invitation, JID inviterJID, BareJID roomJID) {
+				// someone invited me
+				System.out.println(inviterJID + " invited you to room " + roomJID);
+
+				// we are happy and we are joinging
+				try {
+					mucModule.join(invitation, "OurUniqueNickname");
+				} catch (JaxmppException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
 		mucModule.join("test000", "muc.tigase.org", "TesterBot");
+
+
 		Thread.sleep(5000);
 		mucModule.leave(mucModule.getRoom(BareJID.bareJIDInstance("test000", "muc.tigase.org")));
 		Thread.sleep(1000);
