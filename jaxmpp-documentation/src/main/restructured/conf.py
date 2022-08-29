5286e78f4408b49d1fc0a01952e41e55e47e59e9
Qian Luo <qianqianluo@gmail.com>
2022-08-29 02:11:11 -0700

    updated to generate single .po files

--- jaxmpp-documentation/src/main/restructured/conf.py
+++ jaxmpp-documentation/src/main/restructured/conf.py
@@ -72,11 +72,14 @@ html_theme_options = {'collapse_navigation': True,
     'display_version': True,
 }
 
-gettext_compact = False 
+gettext_compact = "jaxmpp_translation"
 language = "zh_CN"
 locale_dirs = ["locale/"]
 
 
-gettext_compact = False 
+gettext_compact = "jaxmpp_translation"
 language = "pl"
 locale_dirs = ["locale/"]
+
+gettext_allow_fuzzy_translations = True
+
